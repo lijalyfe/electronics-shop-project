13e3dffcfdfa3bd4b4e437d3a0af387f5fe61d3b
lija_lyfe <lija6556@list.ru>
2023-06-06 23:39:08 +0500

    add def test_item_class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,17 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-#from src.item import Item
-#def test_item_class():
-    #item1 = Item("Монитор", 15000, 10)
-    #item2 = Item("Мышь", 1000, 50)
-    #item3 = Item("Клавиатура", 3000, 25)
+from src.item import Item
 
-    #assert item1.calculate_total_price() == 150000
-    #assert item2.calculate_total_price() == 50000
-    #assert item3.calculate_total_price() == 75000
+def test_item_class():
+    item1 = Item("Монитор", 15000, 10)
+    item2 = Item("Мышь", 1000, 50)
+    item3 = Item("Клавиатура", 3000, 25)
 
-    #Item.pay_rate = 0.9
+    assert item1.calculate_total_price() == 150000
+    assert item2.calculate_total_price() == 50000
+    assert item3.calculate_total_price() == 75000
 
-    #item3.apply_discount()
+    Item.pay_rate = 0.9
 
-    #assert item3.price == 2700.0
-    #assert Item.all == [item1, item2, item3]
+    item3.apply_discount()
+
+    assert item3.price == 2700.0
+    assert Item.all == [item1, item2, item3]
