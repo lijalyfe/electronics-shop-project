0fa171b9b06fd9f2a7940bc7c00f0f6ee7a51710
lija_lyfe <lija6556@list.ru>
2023-06-15 17:06:53 +0500

    add test_phone.py in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
 from src.item import Item
+from src.phone import Phone
 
 def test_item_class():
     item1 = Item("Монитор", 15000, 10)
@@ -26,3 +28,35 @@ def test_item_repr():
 def test_item_str():
     item = Item("Смартфон", 10000, 20)
     assert str(item) == 'Смартфон'
+
+
+# TestCase#1 на создание экземпляра класса Phone:
+def test__init__():
+    phone = Phone("Iphone XR", 50_000, 10, 2)
+
+    assert phone._name == "Iphone XR"
+    assert phone.price == 50000
+    assert phone.quantity == 10
+    assert phone.number_of_sim == 2
+
+
+# TestCase#2 на корректность возвращаемого значения метода repr:
+def test__repr__():
+    phone = Phone("Iphone XR", 50_000, 10, 2)
+
+    assert repr(phone) == "Phone('Iphone XR', 50000, 10, 2)"
+
+
+# TestCase#3
+def test__add__():
+    phone = Phone("Iphone XR", 50_000, 10, 2)
+    phone2 = Phone("Samsung S21", 40_000, 8, 1)
+    item = Item("Чехол для Iphone XR", 3_000, 20)
+
+    # на корректность сложения двух экземпляров класса Phone:
+    assert (phone + phone2) == 18
+
+    # на корректность сложения экземпляра класса Phone и экземпляра класса Item:
+    assert (phone + item) == 30
+
+
--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,4 +1,5 @@
 from src.phone import Phone
+from src.item import Item
 
 # TestCase#1 на создание экземпляра класса Phone:
 def test__init__():
@@ -17,12 +18,14 @@ def test__repr__():
     assert repr(phone) == "Phone('Iphone XR', 50000, 10, 2)"
 
 
-# TestCase#3 на корректность сложения двух экземпляров класса Phone:
+# TestCase#3
 def test__add__():
-    phone1 = Phone("Iphone XR", 50_000, 10, 2)
+    phone = Phone("Iphone XR", 50_000, 10, 2)
     phone2 = Phone("Samsung S21", 40_000, 8, 1)
+    item = Item("Чехол для Iphone XR", 3_000, 20)
 
-    assert (phone1 + phone2) == 18
+    # на корректность сложения двух экземпляров класса Phone:
+    assert (phone + phone2) == 18
 
-
-
+    # на корректность сложения экземпляра класса Phone и экземпляра класса Item:
+    assert (phone + item) == 30
