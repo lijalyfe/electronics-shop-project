85261066de6503d893ebb841f75626f1705adf24
lija_lyfe <lija6556@list.ru>
2023-06-20 21:50:00 +0500

    add class LanguageMixin

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,3 +4,13 @@ class Keyboard(Item, LanguageMixin):
     """Класс для товара "Клавиатура" """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class LanguageMixin:
+    """Миксин класса для хранения и изменения языка"""
+    LANGUAGES = ['EN','RU']
+    def __init__(self, language = 'EN', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._language = language
+
+
