700c42b75a54fc158ac3c98e148a5ab83d1bf8b1
lija_lyfe <lija6556@list.ru>
2023-06-24 20:53:46 +0500

    add def change_lang

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,30 +1,37 @@
 from src.item import Item
 
-class Keyboard(Item, LanguageMixin):
-    """Класс для товара "Клавиатура" """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
 
 class LanguageMixin:
     """Миксин класса для хранения и изменения языка"""
     LANGUAGES = ['EN','RU']
-    def __init__(self, language = 'EN', *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._language = language
+    def __init__(self):
+        self.__language = "EN"
 
 
     @property
     def language(self):
-        return self._language
+        return self.__language
 
 
     @language.setter
-    def language(self, value):
-        if value in self.LANGUAGES
-            self._language = value
-        else:
+    def language(self, lang):
+        if lang not in self.LANGUAGES:
             raise ValueError("Недопустимый язык")
+        self.__language = lang
 
 
 
+    def change_lang(self):
+        """Метод для изменения языка."""
+        if self.__language == "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+
+class Keyboard(Item, LanguageMixin):
+    """Класс для товара "Клавиатура" """
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+
+    def __str__(self):
+        return super().__str__()
