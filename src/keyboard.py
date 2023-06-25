704ab5c749998b921468d11a9770350565800ec5
lija_lyfe <lija6556@list.ru>
2023-06-25 23:59:58 +0500

    add def set_language

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,37 +3,28 @@ from src.item import Item
 
 class LanguageMixin:
     """Миксин класса для хранения и изменения языка"""
-    LANGUAGES = ["EN","RU"]
-    def __init__(self):
-        self._language = "EN"
-
-
-    @property
-    def language(self):
-        return self._language
-
-
-    @language.setter
-    def language(self, lang):
-        if lang is not None:
-            if lang not in self.LANGUAGES:
-                raise ValueError("Language not supported")
-            self._language = lang
-
-
+    LANGUAGES = ["EN", "RU"]
+    language = "EN"
 
     def change_lang(self):
         """Метод для изменения языка."""
-        if self._language == "EN":
-            self._language = "RU"
+        if self.language == "EN":
+            self.language = "RU"
         else:
-            self._language = "EN"
+            self.language = "EN"
+
 
 class Keyboard(Item, LanguageMixin):
     """Класс для товара "Клавиатура" """
-    def __init__(self, name, price, quantity, language=None):
+    def __init__(self, name, price, quantity, language="EN"):
         super().__init__(name, price, quantity)
+        self.set_language(language)
 
+    def set_language(self, language):
+        """Устанавливает язык клавиатуры."""
+        if language not in self.LANGUAGES:
+            raise ValueError("Language not supported")
+        self.language = language
 
     def __str__(self):
         return super().__str__()
